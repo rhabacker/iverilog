9e457169f70235939cbd6a76e5faed8e3801462c
Ralf Habacker <ralf.habacker@freenet.de>
2026-05-18 12:40:25 +0200

    ivl: move target into separate subdir and convert to automake

--- autoconf.sh
+++ autoconf.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -euo pipefail
 
 #
 # This shell script exists to run autoconf on source distributions
@@ -6,13 +7,5 @@
 # in git, so it is easiest to just run this script whenever needed
 # to generate the configure script.
 #
-# wget -O config.guess 'https://git.savannah.gnu.org/cgit/config.git/plain/config.guess'
-# wget -O config.sub 'https://git.savannah.gnu.org/cgit/config.git/plain/config.sub'
-echo "Autoconf in root..."
+echo "Run autoconf in root directory"
 autoreconf -fi
-
-echo "Precompiling lexor_keyword.gperf"
-gperf -o -i 7 -C -k 1-4,6,9,\$ -H keyword_hash -N check_identifier -t ./lexor_keyword.gperf > lexor_keyword.cc
-
-echo "Precompiling vhdlpp/lexor_keyword.gperf"
-(cd vhdlpp ; gperf -o -i 7 --ignore-case -C -k 1-4,6,9,\$ -H keyword_hash -N check_identifier -t ./lexor_keyword.gperf > lexor_keyword.cc )
